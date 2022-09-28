ec9bc37143e32deb7e5780e8452dfe3e32624a05
Bihan Viranga <bihanviranga@gmail.com>
2022-09-28 16:11:16 +0530

    Play songs from the Discover page
    Implemented PlayPause component for buttons. Added functionality to the
    SongCard component.

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,17 @@ import { useDispatch } from 'react-redux';
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
-const SongCard = ({ song, i }) => {
-  const activeSong = 'test';
+const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
+  const dispatch = useDispatch();
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80
@@ -16,9 +25,26 @@ const SongCard = ({ song, i }) => {
           bg-opacity-50 group-hover:flex
           ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}
         >
-          <PlayPause />
+          <PlayPause
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+          />
         </div>
         <img alt="song album art" src={song.images?.coverart} />
+      </div>
+
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>{song.title}</Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+            {song.subtitle}
+          </Link>
+        </p>
       </div>
     </div>
   );
