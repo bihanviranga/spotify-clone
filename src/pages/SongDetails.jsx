9d3d30b25a4889f6b12453fd6c3a7b705e713c68
Bihan Viranga <bihanviranga@gmail.com>
2022-09-29 15:45:32 +0530

    Implement song details page and related components

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,61 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid: songId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: songData,
+    isFetching: isFetchingSongDetails,
+  } = useGetSongDetailsQuery({ songId });
+  const {
+    data: relatedSongsData,
+    isFetching: isFetchingRelatedSongs,
+    error,
+  } = useGetSongRelatedQuery({ songId });
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, relatedSongsData, i }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingSongDetails || isFetchingRelatedSongs) return <Loader titile="Seaching song details" />;
+
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId="" songData={songData} />
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics</h2>
+        <div className="mt-5">
+          {songData?.sections[1].type === 'LYRICS'
+            ? songData?.sections[1].text.map((line) => (
+              <p className="text-gray-400 text-base my-1">{line}</p>
+            ))
+            : <p className="text-gray-400 text-base my-1">No lyrics found</p>}
+        </div>
+      </div>
+
+      <RelatedSongs
+        data={relatedSongsData}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePlayClick={handlePlayClick}
+        handlePauseClick={handlePauseClick}
+      />
+    </div>
+  );
+};
 
 export default SongDetails;
